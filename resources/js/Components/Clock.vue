e9dc6331713bbc458594a065d81d99cc4ba9a855
John Paul Sim <johnpaul.sim@hmrphils.com>
2023-08-13 02:19:08 +0800

    style : Create Component & direction

---
+++ resources/js/Components/Clock.vue
@@ -0,0 +1,60 @@
+ <script>
+  import moment from 'moment'
+  
+  export default {
+    data () {
+      return {
+        day: moment().format('D'),
+        hour: moment().format('H'),
+        minute: moment().format('m'),
+        second: moment().format('s')
+      }
+    },
+    mounted () {
+      this.timer = setInterval(() => {
+        this.second++
+        if (this.second === 60) {
+          this.minute++
+          this.second = 0
+          if (this.minute === 60) {
+            this.hour++
+            this.minute = 0
+            if (this.hour === 24) {
+              this.day++
+              this.hour = 0
+            }
+          }
+        }
+      }, 1000)
+    },
+    destroyed () {
+      clearInterval(this.timer)
+    }
+  }
+  </script>
+<template>
+   <div class="flex flex-col h-screen">
+        <div class="flex justify-center items-center pt-8">
+            <div class="w-100 text-center">
+                <h1 class="text-8xl font-bold">Clock</h1>
+            </div>
+        </div>
+
+        <div class="flex justify-center items-center text-5xl mt-5">
+            <div class="w-1/5">
+                <span class="text-white-300">{{ hour }}</span>
+                <span class="text-gray-500 ml-2">hours</span>
+            </div>
+            <div class="w-1/5">
+                <span class="text-white-300">{{ minute }}</span>
+                <span class="text-gray-500 ml-2">minute</span>
+            </div>
+            <div class="w-1/5">
+                <span class="text-white-300">{{ second }}</span>
+                <span class="text-gray-500 ml-2">second</span>
+            </div>
+        </div>
+    </div>
+</template>
+  
+ 
--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -33,6 +33,21 @@ const showingNavigationDropdown = ref(false);
                                     Dashboard
                                 </NavLink>
                             </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                    Egant
+                                </NavLink>
+                            </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                    AUX
+                                </NavLink>
+                            </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                    Time Stamp
+                                </NavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
@@ -111,6 +126,11 @@ const showingNavigationDropdown = ref(false);
                     :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
                     class="sm:hidden"
                 >
+                    <div class="pt-2 pb-3 space-y-1">
+                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                            Dashboard
+                        </ResponsiveNavLink>
+                    </div>
                     <div class="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                             Dashboard
