6a3a7a508a0dc79bda6c0f1eed7250ad4f317719
John Paul Sim <johnpaul.sim@hmrphils.com>
2023-08-13 02:12:57 +0800

    feat : Add dependencies & UI :boom:
    - :lipstick: Add the UI and style
     * priemvue style and UI
    - :package: Install dependencies
     * moment
     * primeicons
     * primevue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,12 +1,19 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import Clock from '@/Components/Clock.vue';
+
 </script>
 
 <template>
     <Head title="Dashboard" />
-
+    
     <AuthenticatedLayout>
+        <div class="card flex justify-content-center">
+        <Button label="Check" icon="pi pi-check" />
+    </div>
+
+        <Clock />
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
